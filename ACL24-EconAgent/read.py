cc6de9c41d9bb4842024ab7658e0967f9b1240d0
yhe2 <116295741+codepassionor@users.noreply.github.com>
2025-02-11 02:07:30 -0600

    Delete ACL24-EconAgent/read.py

--- ACL24-EconAgent/read.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# 打开并读取 pkl 文件
-with open('/Users/heyangfan/Desktop/ACL24-EconAgent/data/gpt-3-full-100agents-240months/dialog4ref_36.pkl', 'rb') as f:
-    data = pickle.load(f)
-
-# 打印出文件中的内容
-print(data)
